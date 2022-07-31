01bc97f2a389c9a9674b06ebaa16440a49cf3e3d
Jacob Bourne <jbourne2021@fau.edu>
2022-07-31 16:18:15 -0400

    hopefully

--- modules/lambda_layer/main.tf
+++ modules/lambda_layer/main.tf
@@ -36,7 +36,7 @@ resource "aws_lambda_layer_version" "lambda_layer" {
   description         = "A nice lambda layer stored on S3"
   s3_bucket           = var.lambda_layer_bucket
   s3_key              = "lambda-layers/${var.layer_name}"
-  source_code_hash    = filebase64sha256(data.archive_file.source.output_path)
+  source_code_hash    = filebase64sha256("${path.module}/lambda_layer.zip")
 
   depends_on = [data.archive_file.source]
 }
