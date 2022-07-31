72752b5a8dfd2d4ab74585099f727c8b7e2c950b
Jacob Bourne <jbourne2021@fau.edu>
2022-07-31 15:53:50 -0400

    removed extranious id

--- modules/lambda_layer/outputs.tf
+++ modules/lambda_layer/outputs.tf
@@ -1,25 +1,25 @@
 # Lambda Layer
 output "lambda_layer_arn" {
   description = "The ARN of the Lambda Layer with version"
-  value       = aws_lambda_layer_version.lambda_layer.lambda_layer_arn
+  value       = aws_lambda_layer_version.lambda_layer_arn
 }
 
 output "lambda_layer_layer_arn" {
   description = "The ARN of the Lambda Layer without version"
-  value       = aws_lambda_layer_version.lambda_layer.lambda_layer_layer_arn
+  value       = aws_lambda_layer_version.lambda_layer_layer_arn
 }
 
 output "lambda_layer_created_date" {
   description = "The date Lambda Layer resource was created"
-  value       = aws_lambda_layer_version.lambda_layer.lambda_layer_created_date
+  value       = aws_lambda_layer_version.lambda_layer_created_date
 }
 
 output "lambda_layer_source_code_size" {
   description = "The size in bytes of the Lambda Layer .zip file"
-  value       = aws_lambda_layer_version.lambda_layer.lambda_layer_source_code_size
+  value       = aws_lambda_layer_version.lambda_layer_source_code_size
 }
 
 output "lambda_layer_version" {
   description = "The Lambda Layer version"
-  value       = aws_lambda_layer_version.lambda_layer.lambda_layer_version
+  value       = aws_lambda_layer_version.lambda_layer_version
 }
